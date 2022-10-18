a42910523c2afad9c45660d188289395e24b1258
Jake Hoffman <jakehoffman1160@gmail.com>
2022-10-18 00:38:34 -0400

    Ctrl z and ctrl y done

--- client/src/components/PlaylistCards.js
+++ client/src/components/PlaylistCards.js
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 import SongCard from './SongCard.js'
 import { GlobalStoreContext } from '../store'
+import { useCallback, useEffect } from 'react';
 /*
     This React component lets us edit a loaded list, which only
     happens when we are on the proper route.
@@ -11,6 +12,26 @@ import { GlobalStoreContext } from '../store'
 function PlaylistCards() {
     const { store } = useContext(GlobalStoreContext);
     store.history = useHistory();
+
+    const handleKeyPress = useCallback((event) => {
+        if(event.key === 'z' && event.ctrlKey && store.markedSong === null){
+            store.undo();
+        }
+        if(event.key === 'y' && event.ctrlKey && store.markedSong === null){
+            store.redo();
+        }
+      }, []);
+    
+      useEffect(() => {
+        // attach the event listener
+        document.addEventListener('keydown', handleKeyPress);
+    
+        // remove the event listener
+        return () => {
+          document.removeEventListener('keydown', handleKeyPress);
+        };
+      }, [handleKeyPress]);
+
     return (
         <div id="playlist-cards">
         {
